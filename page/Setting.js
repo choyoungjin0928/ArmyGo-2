9a26e82c9b1e6cbfd1d248bbd7ea75c17c8f3d12
wkd9146 <wkd9146@gmail.com>
2020-11-24 15:11:42 +0900

    Thrid Modified

--- page/Setting.js
+++ page/Setting.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Constants from 'expo-constants';
+import { firebase_db } from "../firebaseConfig"
 import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-community/picker';
 
@@ -8,6 +10,34 @@ export default Setting = ({ navigation }) => {
     const [v1value, setV1alue] = useState("0000.00.00")
     const [v2value, setV2value] = useState("00")
     const [v3value, setV3value] = useState("00")
+
+    useEffect(() => {
+        console.log("Firbase Write")
+    }, [])
+    const Result = () => {
+        const new_history = {
+            value: value,
+            v1value: v1value,
+            v2value: v2value,
+            v3value: v3value,
+        }
+        const user_id = Constants.installationId;
+        console.log(value)
+        console.log(v1value)
+        console.log(v2value)
+        console.log(v3value)
+        firebase_db.ref('/data/' + user_id).set(new_history, function (error) {
+            console.log(error)
+            if (error = null) {
+                navigation.navigate("Main", {
+                    value: value,
+                    v1value: v1value,
+                    v2value: v2value,
+                    v3value: v3value
+                })
+            }
+        });
+    }
 
     return (
         <>
@@ -25,34 +55,30 @@ export default Setting = ({ navigation }) => {
                 <Text>{"\n"}</Text>
                 <Text style={styles.tt}>입대날짜</Text>
                 <TextInput style={styles.tt2} placeholder="2020.11.11" onChangeText={setV1alue} />
-                <TouchableOpacity
-                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
-                >
-                    
-                    <Text>Submit</Text>
-                    <Text>{"\n"}</Text>
-                </TouchableOpacity>
+
+
+                <Text>{"\n"}</Text>
                 <Text style={styles.tt}>포상휴가</Text>
                 <TextInput style={styles.tt2} placeholder="00" onChangeText={setV2value} />
                 <TouchableOpacity
-                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
+                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || Result()}
                 >
-                    <Text>Submit</Text>
+
                     <Text>{"\n"}</Text>
                 </TouchableOpacity>
                 <Text style={styles.tt}>위로휴가</Text>
                 <TextInput style={styles.tt2} placeholder="00" onChangeText={setV3value} />
                 <TouchableOpacity
-                   onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
+                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || Result()}
                 >
+                    <Text>{"\n"}</Text>
                     <Text>Submit</Text>
                 </TouchableOpacity>
             </View>
         </>
     )
-
-
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
