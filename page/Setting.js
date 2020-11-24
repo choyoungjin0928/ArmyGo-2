f7feccd50ca2af24b5c14a9adec2bf00a6bdb44b
wkd9146 <wkd9146@gmail.com>
2020-11-24 13:10:13 +0900

    Second Modified

--- page/Setting.js
+++ page/Setting.js
@@ -4,9 +4,10 @@ import { Picker } from '@react-native-community/picker';
 
 export default Setting = ({ navigation }) => {
 
-    const [value, setValue] = useState("0000.00.00")
-    const [v1value, setV1value] = useState("00")
+    const [value, setValue] = useState("")
+    const [v1value, setV1alue] = useState("0000.00.00")
     const [v2value, setV2value] = useState("00")
+    const [v3value, setV3value] = useState("00")
 
     return (
         <>
@@ -14,7 +15,7 @@ export default Setting = ({ navigation }) => {
                 <Text style={styles.tt}>군 종류</Text>
                 <Picker
                     style={{ height: 50, width: 160 }}
-                    onValueChange={(value) => console.log(value)}>
+                    onValueChange={setValue}>
                     <Picker.Item label="선택해주세요" value="" />
                     <Picker.Item label="육군" value="roka" />
                     <Picker.Item label="해군" value="navi" />
@@ -23,44 +24,33 @@ export default Setting = ({ navigation }) => {
                 </Picker>
                 <Text>{"\n"}</Text>
                 <Text style={styles.tt}>입대날짜</Text>
-                <TextInput style={styles.tt2} placeholder="2020.11.11" onChangeText={setValue} />
+                <TextInput style={styles.tt2} placeholder="2020.11.11" onChangeText={setV1alue} />
                 <TouchableOpacity
-                    onPress={() => navigation.navigate("Main", {
-                        value: 1,
-                        v1value: 2,
-                        v2value: 3
-                    })}
+                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
                 >
+                    
                     <Text>Submit</Text>
                     <Text>{"\n"}</Text>
                 </TouchableOpacity>
                 <Text style={styles.tt}>포상휴가</Text>
-                <TextInput style={styles.tt2} placeholder="00" onChangeText={setV1value} />
+                <TextInput style={styles.tt2} placeholder="00" onChangeText={setV2value} />
                 <TouchableOpacity
-                    onPress={() => navigation.navigate("Second", {
-                        value: 1,
-                        v1value: 2,
-                        v2value: 3
-                    })}
+                    onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
                 >
                     <Text>Submit</Text>
                     <Text>{"\n"}</Text>
                 </TouchableOpacity>
                 <Text style={styles.tt}>위로휴가</Text>
-                <TextInput style={styles.tt2} placeholder="00" onChangeText={setV2value} />
+                <TextInput style={styles.tt2} placeholder="00" onChangeText={setV3value} />
                 <TouchableOpacity
-                    onPress={() => navigation.navigate("Second", {
-                        value: { value },
-                        v1value: { v1value },
-                        v2value: { v2value },
-                        navitation: { navigation }
-                    })}
+                   onPress={() => console.log(value) || console.log(v1value) || console.log(v2value) || console.log(v3value) || navigation.navigate('Main', {value, v1value, v2value, v3value})}
                 >
                     <Text>Submit</Text>
                 </TouchableOpacity>
             </View>
         </>
     )
+
 
 }
 const styles = StyleSheet.create({
